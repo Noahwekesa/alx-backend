25d927ddbaf4d2f8ab5ccf7a2461cc9c3d2846af
noahwekesa <noahwekesa0@gmail.com>
2024-05-22 21:16:35 +0300

    fix issue

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -26,16 +26,18 @@ class LIFOCache(BaseCaching):
             if key in self.cache_data:
                 self.cache_data[key] = item
                 self.order.remove(key)
-        else:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                discard = self.order.pop()
-                del self.cache_data[discard]
-                print("DISCARD:", discard)
-            self.cache_data[key] = item
+            else:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    last = self.order.pop()
+                    del self.cache_data[last]
+                    print("DISCARD: {}".format(last))
+                self.cache_data[key] = item
             self.order.append(key)
 
     def get(self, key):
         """
         Get an item by key
         """
-        return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
