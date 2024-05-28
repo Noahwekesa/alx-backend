1669718f7298ecc99b2f7a608ede99da4c41525f
noahwekesa <noahwekesa0@gmail.com>
2024-05-28 21:50:49 +0300

    test

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,10 +3,8 @@
 Basic Babel
 """
 
+from flask import Flask, render_template
 from flask_babel import Babel
-
-
-babel = Babel()
 
 
 class Config:
@@ -15,3 +13,23 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+@babel.localeselector
+    def get_locale():
+        """Get locale"""
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index() -> str:
+    """Returns a template"""
+    return render_template('1-index.html')
+
+
+if __name__=="__main__":
+    app.run()
