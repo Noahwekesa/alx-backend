ce8726f5dbaf18f525b1112a339eef8acf45dff4
noahwekesa <noahwekesa0@gmail.com>
2024-05-28 21:52:39 +0300

    request

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,7 +3,7 @@
 Basic Babel
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -20,16 +20,18 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
-@babel.localeselector
-    def get_locale():
-        """Get locale"""
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-@app.route('/')
+@babel.localeselector
+def get_locale():
+    """Get locale"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
 def index() -> str:
     """Returns a template"""
-    return render_template('1-index.html')
+    return render_template("1-index.html")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
