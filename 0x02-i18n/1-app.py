61f0d9e2725ff7f1d0eb63742c9aa162589e6ae9
noahwekesa <noahwekesa0@gmail.com>
2024-05-28 21:14:39 +0300

    basic

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+#!/usr/bin/env python3
+"""
+Basic Babel
+"""
+
 from flask_babel import Babel
 
-app = Flask(__name__)
-app.config.from_pyfile("mysettings.cfg")
-babel = Babel(app)
+babel = Babel()
 
 
 class Config:
